865a7cc2f5195c49811c9d07a52c69f63fde5dd2
seohyunn <dhtjgus1356@naver,com>
2023-09-01 19:35:51 +0900

    서현/ 메인 만드는 중,,,,,

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -36,3 +36,30 @@ function fadeInOnScroll() {
   // 초기 로드 시에도 페이드 인 효과 적용을 위해 실행
   fadeInOnScroll();
   
+
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
