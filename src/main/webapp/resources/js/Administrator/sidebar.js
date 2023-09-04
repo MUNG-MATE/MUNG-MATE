23bee8ae1ae7fbbaa3666c76abe2c32ca1ab1b34
Gwang <mungwang1208@gmail.com>
2023-09-04 16:55:17 +0900

    09-04 사이드바 faq 끝

--- src/main/webapp/resources/js/Administrator/sidebar.js
+++ src/main/webapp/resources/js/Administrator/sidebar.js
@@ -24,23 +24,4 @@ $(".gnb>li").click(function () {
     })
     
 });
-const toggleButtons = document.querySelectorAll('.td'); 
 
-toggleButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        const faqAnswerRows = document.querySelectorAll('.faq-answer'); 
-
-        // 현재 클릭한 버튼 다음의 답변 행을 토글하여 보이거나 숨깁니다.
-        faqAnswerRows.forEach((row, rowIndex) => {
-            if (rowIndex === index) {
-                if (row.style.display === 'block' || row.style.display === '') {
-                    row.style.display = 'none';
-                } else {
-                    row.style.display = 'block';
-                }
-            } else {
-                row.style.display = 'none'; 
-            }
-        });
-    });
-});
