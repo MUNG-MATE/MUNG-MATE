a4f1158fba7154bea4c6067dbf2ea422e5dadb23
seohyunn <dhtjgus1356@naver,com>
2023-09-06 16:46:09 +0900

    승남님 받으십쇼,,,,

---
+++ src/main/java/edu/kh/mung/myPage/model/dao/myPageDAO.java
@@ -0,0 +1,8 @@
+package edu.kh.mung.myPage.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class myPageDAO {
+
+}
---
+++ src/main/java/edu/kh/mung/myPage/myPageController.java
@@ -0,0 +1,10 @@
+package edu.kh.mung.myPage;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class myPageController {
+
+	
+	
+}
