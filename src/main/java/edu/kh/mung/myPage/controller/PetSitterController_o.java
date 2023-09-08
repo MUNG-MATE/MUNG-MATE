fa1d992d60741eda64c880419c4e49172d1f2ef3
seohyunn <dhtjgus1356@naver,com>
2023-09-08 17:35:27 +0900

    서현 / 펫시터 일반회원으로 전환 시작

--- src/main/java/edu/kh/mung/myPage/controller/PetSitterController_o.java
+++ src/main/java/edu/kh/mung/myPage/controller/PetSitterController_o.java
@@ -1,14 +1,20 @@
 package edu.kh.mung.myPage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import edu.kh.mung.myPage.model.service.ApplicationService_o;
+
 @Controller
 @SessionAttributes({"loginMember"})
 @RequestMapping("/petsitter")
-public class PetSitterContorller_o {
+public class PetSitterController_o {
+	
+	@Autowired
+	private ApplicationService_o service;
 
 	@GetMapping("/toGeneralApply")
 	public String toGeneralApply() {
