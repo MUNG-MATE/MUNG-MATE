b9fdab9b34340ff0c7943fd9b67fbc7880988b5d
hongseungnam <hsn433@naver.com>
2023-09-09 17:17:10 +0900

    눈밑떨림 드디어 해결

---
+++ src/main/webapp/resources/js/common/headerLast.js
@@ -0,0 +1,7 @@
+const head = document.getElementsByClassName("head one");
+const headArea = document.getElementsByClassName("headArea two");
+const scrollY = window.scrollY;
+
+if(scrollY > 167){
+    
+}
