09675807e32f67faeda6b0a4f0f36c954355860d
jeongheum park <likegym2@naver.com>
2023-09-11 15:34:41 +0900

    야호

--- src/main/java/edu/kh/mung/board/model/dto/Board.java
+++ src/main/java/edu/kh/mung/board/model/dto/Board.java
@@ -1,5 +1,20 @@
 package edu.kh.mung.board.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Board {
 
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardDate;
+	private String boardState;
+	private int reviewStar;
+	private int boardCode;
+	
 }
---
+++ src/main/java/edu/kh/mung/myPage/model/service/PetSitterService_p.java
@@ -0,0 +1,5 @@
+package edu.kh.mung.myPage.model.service;
+
+public interface PetSitterService_p {
+
+}
