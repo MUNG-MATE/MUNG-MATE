6190adcd18233353059caca0940b097624c5a926
hongseungnam <hsn433@naver.com>
2023-09-13 16:55:21 +0900

    승남 커밋

--- src/main/java/edu/kh/mung/reservation/model/dao/ReservationDao_h.java
+++ src/main/java/edu/kh/mung/reservation/model/dao/ReservationDao_h.java
@@ -1,13 +1,23 @@
 package edu.kh.mung.reservation.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import edu.kh.mung.reservation.model.dto.PetSitter;
+import edu.kh.mung.reservation.model.dto.Reservation;
 
 @Repository
 public class ReservationDao_h {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	// 펫시터 조회
+	public List<PetSitter> selectPetSitter(Reservation rs) {
+		return sqlSession.selectList("reservationMapper.selectPetSitter", rs);
+	}
 
 }
--- src/main/java/edu/kh/mung/reservation/model/service/ReservationServiceImpl_h.java
+++ src/main/java/edu/kh/mung/reservation/model/service/ReservationServiceImpl_h.java
@@ -1,9 +1,13 @@
 package edu.kh.mung.reservation.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.kh.mung.reservation.model.dao.ReservationDao_h;
+import edu.kh.mung.reservation.model.dto.PetSitter;
+import edu.kh.mung.reservation.model.dto.Reservation;
 
 @Service
 public class ReservationServiceImpl_h implements ReservationService_h{
@@ -11,4 +15,10 @@ public class ReservationServiceImpl_h implements ReservationService_h{
 	
 	@Autowired
     private ReservationDao_h dao;
+
+	// 펫시터 정보 조회
+	@Override
+	public List<PetSitter> selectPetSitter(Reservation rs) {
+		return dao.selectPetSitter(rs);
+	}
 }
