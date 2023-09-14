c13e6cf82aebd80f5b4eaab542e02709c89aa2d1
yonghyuk <choiyonghyuk97@gmail.com>
2023-09-14 17:52:44 +0900

    2023-09-14 반려견 조회, 등록, 삭제 까지 완료

--- src/main/java/edu/kh/mung/myPage/model/service/myPageService_c.java
+++ src/main/java/edu/kh/mung/myPage/model/service/myPageService_c.java
@@ -1,5 +1,34 @@
 package edu.kh.mung.myPage.model.service;
 
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import edu.kh.mung.myPage.model.dto.Pet;
+
 public interface myPageService_c {
+
+	/** 새로운 반려견 등록하기
+	 * @param profileImage
+	 * @param webPath
+	 * @param filePath
+	 * @param newPet
+	 * @return result
+	 * @throws IOException 
+	 * @throws IllegalStateException 
+	 */
+	int insertPet(MultipartFile profileImage, String webPath, String filePath, Pet newPet) throws IllegalStateException, IOException;
+
+	/** 반려견 정보 삭제하기
+	 * @param petNo
+	 * @return result
+	 */
+	int deletePet(int petNo);
+
+	/** 반려견 번호 조회하기 --> 등록하고 그 번호를 가지고 있어야 하기에
+	 * @param newPet
+	 * @return petNo
+	 */
+	int selectPetNo(Pet newPet);
 
 }
--- src/main/webapp/resources/js/myPage/petInfo.js
+++ src/main/webapp/resources/js/myPage/petInfo.js
@@ -1,65 +1,23 @@
-// 생년월 - option 태그 생성
+const petListdiv = document.getElementById("petListdiv"); // 제일 큰 부분
 
 
-const birth = document.getElementById("birth");
+function deletePet(petNo,petName,petType,petOption,petGender,petProfile){
 
-const date = new Date();
-const year = date.getFullYear();
-if(birth != null){
-
-    for(let i = year; i > year-20; i--) {
     
-        birth.innerHTML += `<option>${i}년</option>`;
-    }
+    fetch("/myPage/petInfo/delete", {
+        method : "POST",
+        headers : {"Content-Type" : "application/json"},
+        body : petNo
+    })
+    .then(resp => resp.text())
+    .then(result => {
+        if(result > 0){
+            alert("소중한 반려견 정보가 삭제되었습니다.");
+            
+            location.reload(true);
 
-    const month = document.getElementById("month");
+        }
+    })
+    .catch(e => {console.log(e)});
     
-    for(let i = 1; i < 13; i++) {
-        month.innerHTML += `<option>${i}월</option>`;
-    }
 }
-
-
-// 이미지 파일 추가
-const inputImage = document.getElementById("inputImage");
-const imageText = document.getElementById("imageText");
-const petImage = document.getElementById("petImage");
-let deleteCheck = -1;
-if(inputImage != null){
-
-    inputImage.addEventListener("change", e => {
-    
-        console.log("test");
-        const maxSize = 1 * 1024 * 1024 * 5; // 이미지 최대 크기 : 5MB
-    
-        const file = e.target.files[0];
-    
-        if(file == undefined) {
-            console.log("파일 선택이 취소됨");
-            deleteCheck = -1; 
-            petImage.setAttribute("src", '/resources/images/reservation/inputpetimage.png');
-            return;
-        }
-    
-        if(file.size > maxSize) { // 선택된 파일의 크기가 최대 크기를 초과한 경우
-            alert("5MB 이하의 이미지를 선택해주세요.");
-            inputImage.value = "";
-            deleteCheck = -1;
-            petImage.setAttribute("src", '/resources/images/reservation/inputpetimage.png');
-            return;
-        }
-    
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = e => {
-    
-            const url = e.target.result;
-            petImage.setAttribute("src", url);
-            document.getElementsByTagName("label")[0].append(img);
-    
-            deleteCheck = 1;
-        }
-    
-    });
-}
-
