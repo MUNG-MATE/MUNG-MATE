6895f40f202c00e671ce6623a09a8db565193401
jnaacode <jnaacode@gmail.com>
2023-09-21 09:35:20 +0900

    보드이미지시퀀스

--- src/main/java/edu/kh/mung/chatting/controller/ChattingController.java
+++ src/main/java/edu/kh/mung/chatting/controller/ChattingController.java
@@ -1,11 +1,15 @@
 package edu.kh.mung.chatting.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -27,4 +31,11 @@ public class ChattingController {
 		
 		return "chatting/chatting";
 	}
+	
+	 // 채팅 읽음 표시
+    @PutMapping("/chatting/updateReadFlag")
+    @ResponseBody
+    public int updateReadFlag(@RequestBody Map<String, Object> paramMap) {
+        return service.updateReadFlag(paramMap);
+    }
 }
